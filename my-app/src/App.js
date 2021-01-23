ed62ee24de75d2d8289efa0f928edccfdd666b18
荒井忠輔 <mr.popo1173@gmail.com>
2021-01-23 09:35:19 +0900

    create class component and functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,65 +1,33 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import { render } from '@testing-library/react';
 
-
-import React, { Component } from 'react'
 //JSXを利用する際は、　import　react　が必要になる
+// import React, { Component } from 'react'
 
-class App extends Component {
-  render(){
-    //こんな書き方もできる
-    // const dom = <h1>Hi</h1>
-    // return dom
+//Appというクラスがコンポーネントを継承している
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={()=> {console.log('クリック')}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-      // return (
-      //   <div>
-      //     Hellow, World
-      //   </div>
-      // );
-      //JSX は以下のように変換される
-      // return React.createElement(
-      //   "div",
-      //   null,
-      //   "hellow, world"
-      // )
-
-     //claa はjsの予約言と被ってしまうため、classNameとなっている。
-    // const greeting = "Hi TOM"
-    // const dom = <h1 className=''>{greeting}</h1>
-    // return dom
-    
-    // return <input type="text" onClick={()=> {console.log('クリック')}}/>
-    
-    //onChange  値の状態が変化することで
-    // return <input type="text" onChange={()=> {console.log('クリック')}}/>
-
-
-    //returnで返すJSXは一つのタグでくくらないとシンタックスエラーになる
-    //だが、利用したくないタグを入れることになってしまう。
-    // return (
-    //   <div>
-    //     <label htmlFor="bar">bar</label>
-    //     <input type="text" onChange={()=> {console.log('クリック')}}/>
-    //   </div>
-    // )
-
-    // そこで<React.Fragment>を利用することで、意図としないタグを入れないようにする。
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={()=> {console.log('クリック')}}/>
-      </React.Fragment>
-    )
-    
-    
-
-  }
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    </div>
+  )
 }
 
-  // 書き方色々
-
-
+const Cat =() => {
+  return <div>MEOW!!!</div>
+}
 
 
 
