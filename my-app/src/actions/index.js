98cb58ffc02fb1b6ca0f4e99c7fd4f13125bb9f7
荒井忠輔 <mr.popo1173@gmail.com>
2021-01-24 12:02:32 +0900

    Learn Redux Action create

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,36 @@
+
+//Action Creatorsを作成し
+//viewを担当するcomponent側に渡すため「export」する
+// export const increment = () => {
+//     return
+//     {
+//         type: 'INCREMENT'
+//     }
+// }
+
+// export const idecrement = () => {
+//     return
+//     {
+//         type: 'DECREMENT'
+//     }
+// }
+
+//↑のreturn を↓のように省略してかける
+// export const increment = () => ({
+//         type: 'INCREMENT'
+// })
+// export const idecrement = () => ({
+//         type: 'DECREMENT'
+// })
+
+
+//Type内の識別子を複数で利用するため変数扱いできる
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const idecrement = () => ({
+    type: DECREMENT
+})
