48ef8028d04d5a4dbb20c9a78c779dc56d57c002
荒井忠輔 <mr.popo1173@gmail.com>
2021-01-24 09:38:28 +0900

    learn propTypes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,37 @@
 import React from 'react'
 
-const App = () => {
+//proptypeをインポートする
+import PropTypes from 'prop-types'
 
-  //簡略化するためにmap 関数を使う
+const App = () => {
   const profiles = [
     {name: "TARO", age: 10},
     {name: "HANAKO", age: 5},
-    {name: "NoName"}
+    {name: "Noname"}
   ]
 
   return (
     <div>
       {
-        //profiles配列をmap関数で管理し引数「profile」に渡す
-        //Warning: Each child in a list should have a unique "key" prop.
-        // ⇨　仮想DOMを使うためユニークなkeyが必要になると注意されるため、引数にindexを入れる
-        // どこからどこまでを描画する指示をkeyで行うため
         profiles.map((profile, index)=> {
-          //profileの値をUserコンポーネントに渡す
-          //key をindex で管理
           return <User name={profile.name}　age={profile.age} key={index}/>
         })
         
       }
-    {/* propsでデータを受け取る */}
-    {
-    /* <User name={"TARO"}　age={10}/>
-    <User name={"HANAKO"}　age={5}/> */
-    }
     </div>
   )
 }
-//props で値を受け取る
 const User =(props) => {
   return <div>Hi Iam {props.name}, and {props.age} years old!!</div>
 }
 
-//propsを受け取るコンポーネントにデフォルトpropsを用意しておく
-User.defalutProps = {
-  age: 1
+//propsの型を指定することで、異なる型が渡ってきたらエラーを返せるようにする
+User.propTypes = {
+  //name はstring型で受け取ることを指定する
+  name: PropTypes.string,
+  //ageという属性を受け取る事を必須にする
+  age:PropTypes.number.isRequired
 }
+
 
 export default App;
